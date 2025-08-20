0b18eb375575f023704b99c1294b77abe72b143f
Strong-Bee <lintangsyahdewo26@gmail.com>
2025-08-21 00:25:48 +0700

    Update file Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,35 @@
 @extends('admin/template')
 @section('isi')
+    <div class="app-card alert alert-dismissible shadow-sm mb-4 border-left-decoration" role="alert">
+        <div class="inner">
+            <div class="app-card-body p-3 p-lg-4">
+                <h3 class="mb-3">Welcome, developer!</h3>
+                <div class="row gx-5 gy-3">
+                    <div class="col-12 col-lg-9">
+
+                        <div>Portal is a free Bootstrap 5 admin dashboard template. The design is simple,
+                            clean and modular so it's a great base for building any modern web app.
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <a class="btn app-btn-primary"
+                            href="https://themes.3rdwavemedia.com/bootstrap-templates/admin-dashboard/portal-free-bootstrap-admin-dashboard-template-for-developers/"><svg
+                                width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-arrow-down me-2"
+                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
+                                <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z" />
+                                <path fill-rule="evenodd"
+                                    d="M8 6a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 10.293V6.5A.5.5 0 0 1 8 6z" />
+                            </svg>Free Download</a>
+                    </div>
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="alert"aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+    {{--  --}}
+
     <div class="row g-4 mb-4">
         <div class="col-6 col-lg-3">
             <div class="app-card app-card-stat shadow-sm h-100">
@@ -13,10 +43,10 @@
                                 d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
                         </svg> 20%
                     </div>
-                </div><!--//app-card-body-->
+                </div>
                 <a class="app-card-link-mask" href="#"></a>
-            </div><!--//app-card-->
-        </div><!--//col-->
+            </div>
+        </div>
 
         <div class="col-6 col-lg-3">
             <div class="app-card app-card-stat shadow-sm h-100">
@@ -30,10 +60,10 @@
                                 d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
                         </svg> 5%
                     </div>
-                </div><!--//app-card-body-->
+                </div>
                 <a class="app-card-link-mask" href="#"></a>
-            </div><!--//app-card-->
-        </div><!--//col-->
+            </div>
+        </div>
         <div class="col-6 col-lg-3">
             <div class="app-card app-card-stat shadow-sm h-100">
                 <div class="app-card-body p-3 p-lg-4">
@@ -41,21 +71,21 @@
                     <div class="stats-figure">23</div>
                     <div class="stats-meta">
                         Open</div>
-                </div><!--//app-card-body-->
+                </div>
                 <a class="app-card-link-mask" href="#"></a>
-            </div><!--//app-card-->
-        </div><!--//col-->
+            </div>
+        </div>
         <div class="col-6 col-lg-3">
             <div class="app-card app-card-stat shadow-sm h-100">
                 <div class="app-card-body p-3 p-lg-4">
                     <h4 class="stats-type mb-1">Invoices</h4>
                     <div class="stats-figure">6</div>
                     <div class="stats-meta">New</div>
-                </div><!--//app-card-body-->
+                </div>
                 <a class="app-card-link-mask" href="#"></a>
-            </div><!--//app-card-->
-        </div><!--//col-->
-    </div><!--//row-->
+            </div>
+        </div>
+    </div>
 
     {{-- Grafik --}}
 
@@ -66,14 +96,14 @@
                     <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Line Chart Example</h4>
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <div class="card-header-action">
                                 <a href="charts.html">More charts</a>
-                            </div><!--//card-header-actions-->
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body p-3 p-lg-4">
                     <div class="mb-3 d-flex">
                         <select class="form-select form-select-sm ms-auto d-inline-flex w-auto">
@@ -86,23 +116,23 @@
                     <div class="chart-container">
                         <canvas id="canvas-linechart"></canvas>
                     </div>
-                </div><!--//app-card-body-->
-            </div><!--//app-card-->
-        </div><!--//col-->
+                </div>
+            </div>
+        </div>
         <div class="col-12 col-lg-6">
             <div class="app-card app-card-chart h-100 shadow-sm">
                 <div class="app-card-header p-3">
                     <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Bar Chart Example</h4>
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <div class="card-header-action">
                                 <a href="charts.html">More charts</a>
-                            </div><!--//card-header-actions-->
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body p-3 p-lg-4">
                     <div class="mb-3 d-flex">
                         <select class="form-select form-select-sm ms-auto d-inline-flex w-auto">
@@ -115,10 +145,10 @@
                     <div class="chart-container">
                         <canvas id="canvas-barchart"></canvas>
                     </div>
-                </div><!--//app-card-body-->
-            </div><!--//app-card-->
-        </div><!--//col-->
-    </div><!--//row-->
+                </div>
+            </div>
+        </div>
+    </div>
 
     {{--  --}}
 
@@ -129,14 +159,14 @@
                     <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Progress</h4>
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <div class="card-header-action">
                                 <a href="#">All projects</a>
-                            </div><!--//card-header-actions-->
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body">
                     <div class="item p-3">
                         <div class="row align-items-center">
@@ -146,15 +176,15 @@
                                     <div class="progress-bar bg-success" role="progressbar" style="width: 25%;"
                                         aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                            </div><!--//col-->
+                            </div>
                             <div class="col-auto">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right"
                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                                 </svg>
-                            </div><!--//col-->
-                        </div><!--//row-->
+                            </div>
+                        </div>
                         <a class="item-link-mask" href="#"></a>
                     </div><!--//item-->
 
@@ -167,15 +197,15 @@
                                     <div class="progress-bar bg-success" role="progressbar" style="width: 34%;"
                                         aria-valuenow="34" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                            </div><!--//col-->
+                            </div>
                             <div class="col-auto">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right"
                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                                 </svg>
-                            </div><!--//col-->
-                        </div><!--//row-->
+                            </div>
+                        </div>
                         <a class="item-link-mask" href="#"></a>
                     </div><!--//item-->
 
@@ -187,15 +217,15 @@
                                     <div class="progress-bar bg-success" role="progressbar" style="width: 68%;"
                                         aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                            </div><!--//col-->
+                            </div>
                             <div class="col-auto">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right"
                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                                 </svg>
-                            </div><!--//col-->
-                        </div><!--//row-->
+                            </div>
+                        </div>
                         <a class="item-link-mask" href="#"></a>
                     </div><!--//item-->
 
@@ -207,35 +237,35 @@
                                     <div class="progress-bar bg-success" role="progressbar" style="width: 52%;"
                                         aria-valuenow="52" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
-                            </div><!--//col-->
+                            </div>
                             <div class="col-auto">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right"
                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                                 </svg>
-                            </div><!--//col-->
-                        </div><!--//row-->
+                            </div>
+                        </div>
                         <a class="item-link-mask" href="#"></a>
                     </div><!--//item-->
 
-                </div><!--//app-card-body-->
-            </div><!--//app-card-->
-        </div><!--//col-->
+                </div>
+            </div>
+        </div>
         <div class="col-12 col-lg-6">
             <div class="app-card app-card-stats-table h-100 shadow-sm">
                 <div class="app-card-header p-3">
                     <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Stats List</h4>
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <div class="card-header-action">
                                 <a href="#">View report</a>
-                            </div><!--//card-header-actions-->
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body p-3 p-lg-4">
                     <div class="table-responsive">
                         <table class="table table-borderless mb-0">
@@ -291,10 +321,10 @@
                             </tbody>
                         </table>
                     </div><!--//table-responsive-->
-                </div><!--//app-card-body-->
-            </div><!--//app-card-->
-        </div><!--//col-->
-    </div><!--//row-->
+                </div>
+            </div>
+        </div>
+    </div>
 
     {{--  --}}
 
@@ -314,22 +344,22 @@
                                 </svg>
                             </div><!--//icon-holder-->
 
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <h4 class="app-card-title">Invoices</h4>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body px-4">
 
                     <div class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
                         aliquet eros vel diam semper mollis.</div>
-                </div><!--//app-card-body-->
+                </div>
                 <div class="app-card-footer p-4 mt-auto">
                     <a class="btn app-btn-secondary" href="#">Create New</a>
                 </div><!--//app-card-footer-->
-            </div><!--//app-card-->
-        </div><!--//col-->
+            </div>
+        </div>
         <div class="col-12 col-lg-4">
             <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
                 <div class="app-card-header p-3 border-bottom-0">
@@ -345,22 +375,22 @@
                                 </svg>
                             </div><!--//icon-holder-->
 
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <h4 class="app-card-title">Apps</h4>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body px-4">
 
                     <div class="intro">Pellentesque varius, elit vel volutpat sollicitudin, lacus quam
                         efficitur augue</div>
-                </div><!--//app-card-body-->
+                </div>
                 <div class="app-card-footer p-4 mt-auto">
                     <a class="btn app-btn-secondary" href="#">Create New</a>
                 </div><!--//app-card-footer-->
-            </div><!--//app-card-->
-        </div><!--//col-->
+            </div>
+        </div>
         <div class="col-12 col-lg-4">
             <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
                 <div class="app-card-header p-3 border-bottom-0">
@@ -376,21 +406,21 @@
                                 </svg>
                             </div><!--//icon-holder-->
 
-                        </div><!--//col-->
+                        </div>
                         <div class="col-auto">
                             <h4 class="app-card-title">Tools</h4>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
+                        </div>
+                    </div>
+                </div>
                 <div class="app-card-body px-4">
 
                     <div class="intro">Sed maximus, libero ac pharetra elementum, turpis nisi molestie
                         neque, et tincidunt velit turpis non enim.</div>
-                </div><!--//app-card-body-->
+                </div>
                 <div class="app-card-footer p-4 mt-auto">
                     <a class="btn app-btn-secondary" href="#">Create New</a>
                 </div><!--//app-card-footer-->
-            </div><!--//app-card-->
-        </div><!--//col-->
-    </div><!--//row-->
+            </div>
+        </div>
+    </div>
 @endsection
